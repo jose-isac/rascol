0a50787509584d8bc84376e7490fa3ef6cc9d20b
Rasquirrel <jose-0a4@protonmail.com>
2023-07-31 21:24:20 -0300

    Add the ascii art sample

---
+++ art.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+ascii_art = """
+            
+                      WWWWWWWWWW                 dCCb      
+                     WCCCCCCCCCCW               dC  Cb
+                    WCCCCC+++CCCCW              RC  Cb    daab     dssss    dCCCCb     dooob    FF
+                    WCCCC+++++CCCWbb            RCCb     da  ab   cc       dCC   Cb   do   ob   FF
+                    WCCCCC+++CCCC!!!!!!         RC H     FaaaaJ    $$$     OC         Oc   cO   FF
+                     WCCCCCCCCCC!!!!!!!!        RC CH    Fa  aJ      $$b   dCC   Cb   dc   cb   FF
+                      WCCCCCCCCC!!!!!!          RC CH    Fa  aJ   ssssb     dOCCCb     dooob    FFFFFF
+                    WMFFFFFFFFFddd
+                  WMFFFFFFFFFFWW
+                WMFFFFFFFFFFFFWW                           \x1B[3mSua assistente de terminal\x1B[0m.
+              WMFFFFFFFFFFFMFWWWW                                   Alpha 0.2 
+             MWWFFFEEEEEEFMFFFFW
+           MWFFFFEEEEEEEEMFFFWM
+        MMWFFFFFFMEEEEEMMFFFW
+     WWWFFFFFFFFFFFFFFFFFFMW
+      WWWFFFFFFFFFFFFFFFFW
+         MMMMMMMMMMMMMMM
+"""
+
+
+print(ascii_art)
